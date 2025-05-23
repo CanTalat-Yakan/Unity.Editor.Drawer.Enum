bb46748db52104ac2e6a1d0849ee4dc5fd8279f7
CanTalat-Yakan <57494543+CanTalat-Yakan@users.noreply.github.com>
2025-05-23 11:36:52 +0200

    ⠀⠀⠀⠀⠀

--- Editor/EnumDrawer.cs
+++ Editor/EnumDrawer.cs
@@ -5,9 +5,25 @@ using System;
 
 namespace UnityEssentials
 {
+    /// <summary>
+    /// Provides a custom property drawer for enumerations in the Unity Editor, enabling enhanced functionality such as
+    /// search popups.
+    /// </summary>
+    /// <remarks>This class extends <see cref="PropertyDrawer"/> to customize the rendering and interaction of
+    /// enumeration fields in the Unity Inspector. It supports features like displaying a searchable popup for selecting
+    /// enumeration values, improving usability for enums with many options.</remarks>
     [CustomPropertyDrawer(typeof(Enum), true)]
     public class EnumDrawer : PropertyDrawer
     {
+        /// <summary>
+        /// Renders a custom GUI for a serialized property, providing a searchable popup for enum values.
+        /// </summary>
+        /// <remarks>If the property represents an enum type, a searchable popup is displayed to allow
+        /// users to select an enum value. If an error occurs or the property is not an enum, the default property field
+        /// is rendered instead.</remarks>
+        /// <param name="position">The rectangle on the screen to use for the property GUI.</param>
+        /// <param name="property">The serialized property to render.</param>
+        /// <param name="label">The label to display alongside the property field.</param>
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
             EditorGUI.BeginProperty(position, label, property);
@@ -25,9 +41,18 @@ namespace UnityEssentials
             EditorGUI.EndProperty();
         }
 
+        /// <summary>
+        /// Determines the underlying enum type associated with the specified serialized property.
+        /// </summary>
+        /// <remarks>This method is typically used to retrieve the enum type for properties that are
+        /// serialized in Unity's editor. It handles cases where the property represents an array or a generic
+        /// collection of enums.</remarks>
+        /// <param name="property">The serialized property for which to determine the enum type.</param>
+        /// <returns>The <see cref="Type"/> representing the enum type of the serialized property.  If the property is an array
+        /// or a generic collection, the method returns the element or generic argument type.</returns>
         private Type GetEnumType(SerializedProperty property)
         {
-            Type enumType = fieldInfo.FieldType;
+            var enumType = fieldInfo.FieldType;
             if (enumType.IsArray)
                 enumType = enumType.GetElementType();
             else if (enumType.IsGenericType)
@@ -35,9 +60,27 @@ namespace UnityEssentials
             return enumType;
         }
 
+        /// <summary>
+        /// Retrieves the current value of an enumeration property as an <see cref="Enum"/> instance.
+        /// </summary>
+        /// <param name="enumType">The type of the enumeration to which the property belongs. Must be a valid enumeration type.</param>
+        /// <param name="property">The serialized property containing the enumeration value. The property must represent an enumeration field.</param>
+        /// <returns>An <see cref="Enum"/> instance representing the current value of the enumeration property.</returns>
         private Enum GetCurrentValue(Type enumType, SerializedProperty property) =>
             (Enum)Enum.ToObject(enumType, property.enumValueIndex);
 
+        /// <summary>
+        /// Displays a popup UI element for selecting an enumeration value.
+        /// </summary>
+        /// <remarks>This method renders a labeled popup control in the Unity Editor, allowing users to
+        /// select a value from the specified enumeration. The label is optional and will only be displayed if <paramref
+        /// name="label"/> is not <see cref="GUIContent.none"/>.</remarks>
+        /// <param name="position">The screen position and size of the popup control.</param>
+        /// <param name="property">The serialized property associated with the selected enumeration value.</param>
+        /// <param name="label">The label to display next to the popup control. Use <see cref="GUIContent.none"/> to omit the label.</param>
+        /// <param name="enumType">The type of the enumeration to display in the popup.</param>
+        /// <param name="currentValue">The currently selected enumeration value.</param>
+        /// <param name="enumNames">An array of enumeration names to display in the popup.</param>
         private void DrawSearchPopup(Rect position, SerializedProperty property, GUIContent label, Type enumType, Enum currentValue, string[] enumNames)
         {
             var labelPosition = new Rect(position);
@@ -54,6 +97,17 @@ namespace UnityEssentials
             EnumPopup(buttonPosition, currentValue, enumType, property);
         }
 
+        /// <summary>
+        /// Displays a popup menu for selecting a value from an enumeration.
+        /// </summary>
+        /// <remarks>This method creates a popup menu styled for use in the Unity Editor. It allows users
+        /// to select a value from the specified enumeration. The popup is triggered by a button click or a keyboard
+        /// interaction within the specified rectangle.</remarks>
+        /// <param name="position">The screen rectangle that defines the position and size of the popup menu.</param>
+        /// <param name="currentValue">The currently selected enumeration value. This determines the initial selection in the popup.</param>
+        /// <param name="enumType">The type of the enumeration to display in the popup. Must be a valid <see cref="System.Enum"/> type.</param>
+        /// <param name="onValueChanged">A callback action that is invoked when the user selects a new value from the popup. The selected value is
+        /// passed as a parameter to the callback.</param>
         public static void EnumPopup(Rect position, Enum currentValue, Type enumType, Action<Enum> onValueChanged)
         {
             var buttonText = ObjectNames.NicifyVariableName(currentValue.ToString());
@@ -63,21 +117,68 @@ namespace UnityEssentials
                 EnumSearchPopup.Show(position, enumType, currentValue, onValueChanged);
         }
 
+        /// <summary>
+        /// Displays a popup menu for selecting a value from the specified enumeration type.
+        /// </summary>
+        /// <remarks>This method is typically used in custom editor scripts to render an enumeration
+        /// selection popup in the Unity Editor. The selected value is automatically applied to the provided serialized
+        /// property.</remarks>
+        /// <param name="position">The screen rectangle that defines the position and size of the popup menu.</param>
+        /// <param name="currentValue">The currently selected enumeration value to display in the popup.</param>
+        /// <param name="enumType">The type of the enumeration to display in the popup. Must be a valid <see cref="System.Enum"/> type.</param>
+        /// <param name="property">The serialized property to update with the selected enumeration value.</param>
         public static void EnumPopup(Rect position, Enum currentValue, Type enumType, SerializedProperty property) =>
             EnumPopup(position, currentValue, enumType, (newValue) => SetEnumValue(property, newValue, enumType));
 
+        /// <summary>
+        /// Displays a popup menu for selecting a value from an enumeration.
+        /// </summary>
+        /// <remarks>This method provides a strongly-typed interface for displaying a popup menu for
+        /// enumeration values.  The generic type parameter <typeparamref name="T"/> ensures type safety and eliminates
+        /// the need for casting.</remarks>
+        /// <typeparam name="T">The type of the enumeration. Must be a valid <see langword="enum"/> type.</typeparam>
+        /// <param name="position">The screen rectangle that defines the position and size of the popup menu.</param>
+        /// <param name="currentValue">The currently selected enumeration value. Determines the initial selection in the popup.</param>
+        /// <param name="onValueChanged">A callback action invoked when the user selects a new value from the popup.  The selected value is passed as
+        /// a parameter of type <typeparamref name="T"/>.</param>
         public static void EnumPopup<T>(Rect position, Enum currentValue, Action<T> onValueChanged) where T : Enum =>
             EnumPopup(position, currentValue, typeof(T), (newValue) => onValueChanged((T)newValue));
 
+        /// <summary>
+        /// Displays a popup menu for selecting a value from an enumeration of type <typeparamref name="T"/>.
+        /// </summary>
+        /// <remarks>This method provides a convenient way to render a dropdown menu for selecting
+        /// enumeration values in a graphical user interface. The generic type parameter <typeparamref name="T"/>
+        /// ensures type safety by restricting the popup to a specific enumeration type.</remarks>
+        /// <typeparam name="T">The enumeration type to display in the popup. Must be a valid <see langword="enum"/> type.</typeparam>
+        /// <param name="position">The screen rectangle that defines the position and size of the popup menu.</param>
+        /// <param name="currentValue">The currently selected enumeration value. This determines the initial selection in the popup.</param>
         public static void EnumPopup<T>(Rect position, Enum currentValue) where T : Enum =>
             EnumPopup<T>(position, currentValue, (newValue) => currentValue = newValue);
 
+        /// <summary>
+        /// Sets the value of a serialized property to the specified enumeration value.
+        /// </summary>
+        /// <remarks>This method updates the serialized property to reflect the specified enumeration
+        /// value.  Ensure that the <paramref name="property"/> represents an enumeration field and that the provided 
+        /// <paramref name="newValue"/> is valid for the specified <paramref name="enumType"/>.</remarks>
+        /// <param name="property">The serialized property whose value is to be set. This property must represent an enumeration.</param>
+        /// <param name="newValue">The new enumeration value to assign to the property.</param>
+        /// <param name="enumType">The type of the enumeration. This must match the type of the serialized property.</param>
         public static void SetEnumValue(SerializedProperty property, Enum newValue, Type enumType)
         {
             var newIndex = Array.IndexOf(Enum.GetValues(enumType), newValue);
             SetEnumValue(property, newIndex);
         }
 
+        /// <summary>
+        /// Sets the value of an enum property to the specified index.
+        /// </summary>
+        /// <remarks>If the specified <paramref name="property"/> is <see langword="null"/> or the current
+        /// enum value index is already equal to <paramref name="newIndex"/>, no changes are made. After setting the new
+        /// index, the method applies the modified properties to the serialized object.</remarks>
+        /// <param name="property">The <see cref="SerializedProperty"/> representing the enum field. Must not be <see langword="null"/>.</param>
+        /// <param name="newIndex">The new index to set for the enum value. Must be a valid index for the enum.</param>
         public static void SetEnumValue(SerializedProperty property, int newIndex)
         {
             if (property != null && property.enumValueIndex != newIndex)
--- Editor/EnumSearchPopupWindow.cs
+++ Editor/EnumSearchPopupWindow.cs
@@ -6,6 +6,13 @@ using UnityEngine;
 
 namespace UnityEssentials
 {
+    /// <summary>
+    /// Provides a popup window for selecting an enumeration value with a searchable list.
+    /// </summary>
+    /// <remarks>This class is designed to display a dropdown-style popup for selecting a value from an
+    /// enumeration. It supports searching through the enum values and highlights the currently selected or hovered
+    /// item. The popup is typically used in custom Unity editor tools to enhance user experience when working with
+    /// enums.</remarks>
     public class EnumSearchPopup : EditorWindow
     {
         private const float LineHeight = 22f;
@@ -28,8 +35,18 @@ namespace UnityEssentials
         private static readonly Color s_highlightColorPro = new Color(0.24f, 0.37f, 0.58f);
         private static readonly Color s_highlightColorLight = new Color(0.22f, 0.44f, 0.9f);
 
-        private bool hasInitializedScrollPosition;
-
+        /// <summary>
+        /// Displays a popup window for selecting a value from an enumeration.
+        /// </summary>
+        /// <remarks>The popup window is displayed as a dropdown below the specified button rectangle. It
+        /// dynamically adjusts its size based on the available screen space and the number of enumeration values. The
+        /// caller must ensure that <paramref name="enumType"/> is a valid enumeration type; otherwise, the behavior is
+        /// undefined.</remarks>
+        /// <param name="buttonRect">The screen-space rectangle of the button that triggers the popup. Used to position the popup window.</param>
+        /// <param name="enumType">The type of the enumeration to display in the popup. Must be a valid <see cref="System.Enum"/> type.</param>
+        /// <param name="currentValue">The currently selected enumeration value. The popup will highlight this value initially.</param>
+        /// <param name="onValueSelected">A callback action invoked when a new enumeration value is selected. The selected value is passed as a
+        /// parameter to this action.</param>
         public static void Show(Rect buttonRect, Type enumType, Enum currentValue, Action<Enum> onValueSelected)
         {
             var window = CreateInstance<EnumSearchPopup>();
@@ -40,10 +57,10 @@ namespace UnityEssentials
             window._searchString = string.Empty;
             window._hoverIndex = window._currentIndex;
 
-            Vector2 windowPosition = GUIUtility.GUIToScreenPoint(buttonRect.position + new Vector2(0, buttonRect.height));
-            float availableHeight = Screen.currentResolution.height - windowPosition.y;
+            var windowPosition = GUIUtility.GUIToScreenPoint(buttonRect.position + new Vector2(0, buttonRect.height));
+            var availableHeight = Screen.currentResolution.height - windowPosition.y;
 
-            float contentHeight = Mathf.Min(
+            var contentHeight = Mathf.Min(
                 window.CalculateContentHeight(),
                 MaxWindowHeight,
                 availableHeight);
@@ -54,12 +71,18 @@ namespace UnityEssentials
                 Mathf.Max(buttonRect.width + Padding * 2, MinWindowWidth),
                 contentHeight + Padding * 2);
 
-            Vector2 windowSize = new Vector2(Mathf.Max(buttonRect.width, MinWindowWidth), contentHeight);
+            var windowSize = new Vector2(Mathf.Max(buttonRect.width, MinWindowWidth), contentHeight);
             window.ShowAsDropDown(GUIUtility.GUIToScreenRect(buttonRect), windowSize);
             window.Focus();
             window.ScrollToCurrentItem();
         }
 
+        /// <summary>
+        /// Handles the rendering and interaction logic for the graphical user interface (GUI).
+        /// </summary>
+        /// <remarks>This method processes keyboard and mouse input, renders the search field and enum
+        /// list,  and updates the hover index when the search string changes. It ensures that the GUI  remains
+        /// responsive and visually consistent during user interactions.</remarks>
         public void OnGUI()
         {
             string previousSearch = _searchString;
@@ -82,9 +105,21 @@ namespace UnityEssentials
             }
         }
 
+        /// <summary>
+        /// Handles the event when the control loses focus.
+        /// </summary>
+        /// <remarks>This method is typically used to perform cleanup or finalize operations  when the
+        /// control is no longer the active element.</remarks>
         public void OnLostFocus() =>
             Close();
 
+        /// <summary>
+        /// Handles keyboard input events to navigate and interact with a filtered list of items.
+        /// </summary>
+        /// <remarks>This method processes key events such as arrow keys for navigation, the Enter key for
+        /// selection,  and the Escape key for closing the interface. It updates the hover index, scrolls to the
+        /// selected  item, and repaints the UI as necessary. The method only acts on key down events and ignores input 
+        /// if the filtered list is empty.</remarks>
         private void HandleKeyboardInput()
         {
             if (Event.current.type == EventType.KeyDown)
@@ -126,6 +161,13 @@ namespace UnityEssentials
             }
         }
 
+        /// <summary>
+        /// Handles mouse movement events to update the hover index and trigger a repaint if necessary.
+        /// </summary>
+        /// <remarks>This method processes mouse movement and layout events to determine if the mouse
+        /// position has changed. If the mouse is within the content area, it calculates the index of the item being
+        /// hovered over and updates the hover index accordingly. A repaint is triggered when the hover index
+        /// changes.</remarks>
         private Vector2 _previousMousePosition;
         private void HandleMouseMovement()
         {
@@ -147,6 +189,12 @@ namespace UnityEssentials
             }
         }
 
+        /// <summary>
+        /// Scrolls to the currently selected item in the filtered list, if it exists.
+        /// </summary>
+        /// <remarks>This method checks whether the current index is part of the filtered indices.  If it
+        /// is, the hover index is updated to match the current index, and the view  is scrolled to the corresponding
+        /// item.</remarks>
         private void ScrollToCurrentItem()
         {
             var filtered = GetFilteredIndices();
@@ -157,11 +205,19 @@ namespace UnityEssentials
             }
         }
 
+        /// <summary>
+        /// Scrolls the view to bring the specified item into focus.
+        /// </summary>
+        /// <remarks>If the specified <paramref name="index"/> is not present in the filtered list, the
+        /// method does nothing.  The item is centered in the scroll view if possible, taking into account the view's
+        /// height and padding.</remarks>
+        /// <param name="index">The zero-based index of the item to scroll to. Must correspond to a valid item in the filtered list.</param>
         private void ScrollToItem(int index)
         {
             var filtered = GetFilteredIndices();
             var itemIndex = filtered.IndexOf(index);
-            if (itemIndex == -1) return;
+            if (itemIndex == -1)
+                return;
 
             var itemPosition = itemIndex * LineHeight;
             var scrollViewHeight = position.height - LineHeight - 2 * Padding; // Account for search field and padding
@@ -171,6 +227,14 @@ namespace UnityEssentials
             _scrollPosition.y = Mathf.Clamp(itemPosition - (scrollViewHeight / 2), 0, maxScroll);
         }
 
+        /// <summary>
+        /// Draws a bordered area with a background color and renders the specified content inside it.
+        /// </summary>
+        /// <remarks>The border and background colors are determined based on the current editor theme
+        /// (light or dark mode). The method ensures that the content is drawn within a properly padded area inside the
+        /// border.</remarks>
+        /// <param name="drawContent">A delegate that defines the content to be drawn inside the bordered area. This action is invoked within the
+        /// inner area of the border.</param>
         private void DrawBorder(Action drawContent)
         {
             var borderColor = EditorGUIUtility.isProSkin ? s_borderColorPro : s_borderColorLight;
@@ -184,6 +248,12 @@ namespace UnityEssentials
             GUILayout.EndArea();
         }
 
+        /// <summary>
+        /// Displays a scrollable list of enum values, allowing the user to select one.
+        /// </summary>
+        /// <remarks>This method renders a filtered list of enum values in a scrollable view. Each item in
+        /// the list can be selected by clicking on it, which updates the current selection and closes the list. The
+        /// appearance of the items changes based on their selection or hover state.</remarks>
         private void DrawEnumList()
         {
             var filteredIndices = GetFilteredIndices();
@@ -213,6 +283,13 @@ namespace UnityEssentials
 
             GUILayout.EndScrollView();
         }
+
+        /// <summary>
+        /// Draws the background of an item within a specified rectangular area.
+        /// </summary>
+        /// <param name="rect">The rectangular area where the background will be drawn.</param>
+        /// <param name="highlighted"><see langword="true"/> to draw the background with a highlighted color; otherwise, the background will not
+        /// be drawn.</param>
         private void DrawItemBackground(Rect rect, bool highlighted)
         {
             if (highlighted)
@@ -222,19 +299,38 @@ namespace UnityEssentials
             }
         }
 
+        /// <summary>
+        /// Draws the specified text within the given rectangular area, applying a visual style that optionally
+        /// highlights the text.
+        /// </summary>
+        /// <remarks>The method uses a custom <see cref="GUIStyle"/> to render the text, with alignment
+        /// set to the left and padding applied. If <paramref name="highlighted"/> is <see langword="true"/>, the text
+        /// color is set to white.</remarks>
+        /// <param name="rect">The rectangular area in which to draw the text.</param>
+        /// <param name="text">The text to be displayed. The text will be formatted using <see
+        /// cref="ObjectNames.NicifyVariableName(string)"/>.</param>
+        /// <param name="highlighted">A value indicating whether the text should be highlighted.  <see langword="true"/> to apply a highlighted
+        /// style; otherwise, <see langword="false"/>.</param>
         private void DrawItemText(Rect rect, string text, bool highlighted)
         {
+            var color = highlighted ? Color.white : EditorStyles.label.normal.textColor;
             var style = new GUIStyle(EditorStyles.label)
             {
                 alignment = TextAnchor.MiddleLeft,
                 padding = new RectOffset(10, 0, 0, 0),
-                normal = { textColor = highlighted ? Color.white : EditorStyles.label.normal.textColor },
-                hover = { textColor = highlighted ? Color.white : EditorStyles.label.normal.textColor }
+                normal = { textColor = color },
+                hover = { textColor = color }
             };
 
             GUI.Label(rect, ObjectNames.NicifyVariableName(text), style);
         }
 
+        /// <summary>
+        /// Renders a search field in the editor toolbar and sets focus to it.
+        /// </summary>
+        /// <remarks>This method creates a horizontal toolbar layout containing a text field styled as a
+        /// search field. The text field is assigned a control name of "SearchField" to allow programmatic focus
+        /// management.</remarks>
         private void DrawSearchField()
         {
             GUILayout.BeginHorizontal(EditorStyles.toolbar);
@@ -244,14 +340,22 @@ namespace UnityEssentials
             EditorGUI.FocusTextInControl("SearchField");
         }
 
-        private float CalculateContentHeight()
-        {
-            var itemCount = GetFilteredIndices().Count;
-            return LineHeight + // Search field height
-                   (itemCount * LineHeight) + // Items height
-                   1; // Additional padding
-        }
+        /// <summary>
+        /// Calculates the total height of the content based on the line height and the number of filtered indices.
+        /// </summary>
+        /// <returns>The total height of the content as a floating-point value. This includes the base line height,  the height
+        /// contributed by the filtered indices, and an additional offset of 1.</returns>
+        private float CalculateContentHeight() =>
+            LineHeight + (GetFilteredIndices().Count * LineHeight) + 1;
 
+        /// <summary>
+        /// Retrieves a list of indices from the enumeration names that match the current search string.
+        /// </summary>
+        /// <remarks>The method compares the search string against both the raw and nicified versions of
+        /// the enumeration names. A match is determined if the search string is a substring of either version, ignoring
+        /// case. If the search string is empty or null, all indices are included in the result.</remarks>
+        /// <returns>A list of indices corresponding to enumeration names that match the search string.  The list will contain
+        /// all indices if the search string is empty or null.</returns>
         private List<int> GetFilteredIndices()
         {
             var lowerSearch = _searchString.ToLower();
@@ -272,6 +376,14 @@ namespace UnityEssentials
             return indices;
         }
 
+        /// <summary>
+        /// Sets the selected enumeration value based on the specified index and invokes the associated callback.
+        /// </summary>
+        /// <remarks>This method retrieves the enumeration value at the specified index, invokes the
+        /// callback with the selected value,  and then closes the current context. If an invalid index is provided, the
+        /// method will fail silently.</remarks>
+        /// <param name="index">The zero-based index of the enumeration value to select. Must be within the bounds of the enumeration values
+        /// array.</param>
         private void SetEnumValue(int index)
         {
             try
